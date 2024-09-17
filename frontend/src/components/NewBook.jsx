c2b18fe8583c76aaafd21d34b23ec483e419272d
LukaAlhonen <luka.alhonen@protonmail.com>
2024-09-17 20:57:24 +0300

    Solution for 8.10

--- frontend/src/components/NewBook.jsx
+++ frontend/src/components/NewBook.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries";
 
 const NewBook = () => {
   const [title, setTitle] = useState("");
@@ -7,10 +9,18 @@ const NewBook = () => {
   const [genre, setGenre] = useState("");
   const [genres, setGenres] = useState([]);
 
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+  });
+
   const submit = async (event) => {
     event.preventDefault();
 
     console.log("add book...");
+
+    addBook({
+      variables: { title, author, published: parseInt(published, 10), genres },
+    });
 
     setTitle("");
     setPublished("");
